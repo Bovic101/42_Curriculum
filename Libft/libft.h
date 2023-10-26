cf635d9a0b04ecc491aef9f16db7e30f52fd1f7e
vodebunm <vodebunm@student.42heilbronn.de>
2023-10-26 20:33:15 +0200

    updated version 9

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 02:10:25 by vodebunm          #+#    #+#             */
-/*   Updated: 2023/10/26 16:00:01 by vodebunm         ###   ########.fr       */
+/*   Updated: 2023/10/26 19:57:24 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ char				*ft_strchr(const char *s, int c);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 char				*ft_strrchr(const char *s, int c);
 int					ft_strcmp(const char *s1, const char *s2, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+void				*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
