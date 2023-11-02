d491cbe5b7629ca1dfae9ae80597b718bf05cb1b
vodebunm <vodebunm@student.42heilbronn.de>
2023-11-02 19:51:05 +0100

    ft_lstnew Update

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:09:57 by vodebunm          #+#    #+#             */
-/*   Updated: 2023/11/02 18:11:12 by vodebunm         ###   ########.fr       */
+/*   Updated: 2023/11/02 19:45:35 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*s1;
+	t_list	*s;
 
-	s1 = (t_list *)malloc(sizeof(t_list));
-	if (!s1)
+	s = (t_list *)malloc(sizeof(t_list));
+	if (!s)
 		return (NULL);
-	s1->content = content;
-	s1->next = NULL;
-	return (s1);
+	s->content = content;
+	s->next = NULL;
+	return (s);
 }
